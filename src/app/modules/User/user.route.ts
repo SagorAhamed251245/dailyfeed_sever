1be16b1feb95b0619aa254f1eb07768422338a5c
Sagor Ahamed <104268039+SagorAhamed251245@users.noreply.github.com>
2024-09-30 18:55:47 +0600

    auth

--- src/app/modules/User/user.route.ts
+++ src/app/modules/User/user.route.ts
@@ -4,36 +4,12 @@ import auth from '../../middlewares/auth';
 import validateRequest from '../../middlewares/validateRequest';
 import { upload } from '../../utils/sendImageToCloudinary';
 import { createAdminValidationSchema } from '../Admin/admin.validation';
-import { createFacultyValidationSchema } from '../Post/post.validation';
+
 import { USER_ROLE } from './user.constant';
 import { UserControllers } from './user.controller';
 import { UserValidation } from './user.validation';
 
 const router = express.Router();
-
-router.post(
-  '/create-student',
-  auth(USER_ROLE.superAdmin, USER_ROLE.admin),
-  upload.single('file'),
-  (req: Request, res: Response, next: NextFunction) => {
-    req.body = JSON.parse(req.body.data);
-    next();
-  },
-  validateRequest(createStudentValidationSchema),
-  UserControllers.createStudent,
-);
-
-router.post(
-  '/create-faculty',
-  auth(USER_ROLE.superAdmin, USER_ROLE.admin),
-  upload.single('file'),
-  (req: Request, res: Response, next: NextFunction) => {
-    req.body = JSON.parse(req.body.data);
-    next();
-  },
-  validateRequest(createFacultyValidationSchema),
-  UserControllers.createFaculty,
-);
 
 router.post(
   '/create-admin',
